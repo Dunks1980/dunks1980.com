6e370f8a5ab57512359708d256653558ee9ce0f3
Ian Dunkerley <dunks1980@gmail.com>
2024-08-27 18:16:18 +0100

    Site update

--- main.js
+++ main.js
@@ -1,35 +1,5 @@
-import bay from 'https://cdn.jsdelivr.net/npm/@dunks1980/bay.js@latest/bay.min.mjs';
+import bay from "https://cdn.jsdelivr.net/npm/@dunks1980/bay.js@3.3.10/bay.min.mjs";
+//import bay from "http://127.0.0.1:3001/bay.mjs";
+import modal from 'https://cdn.jsdelivr.net/npm/@dunks1980/bay.js@3.3.10/components/modal.mjs';
 bay();
-fetch("./components/welcome.html")
-  .then(response => response.text())
-  .then(html => {
-    bay.create("d-welcome", html, ["prop-color"]);
-  });
-
-
-function update(e) {
-  e.stopPropagation(); // Prevent event propagation
-
-  var x = e.clientX;
-  var y = e.clientY;
-  var parentRect = document.body.getBoundingClientRect(); // Use document.body instead of child elements
-  var offsetX = x - parentRect.left; // Calculate offsetX relative to the main body
-  var offsetY = y - parentRect.top; // Calculate offsetY relative to the main body
-
-  // Get the position of whole container 
-  var containerRect = document.querySelector('.whole-container').getBoundingClientRect();
-  var containerX = containerRect.left;
-  var containerY = containerRect.top;
-  offsetX = offsetX - containerX;
-  offsetY = offsetY - containerY;
-  document.documentElement.style.setProperty('--cX', offsetX + 'px');
-  document.documentElement.style.setProperty('--cY', offsetY + 'px');
-
-  // Calculate shadow position opposite to the light
-  var shadowX = (-offsetX + parentRect.width / 2) / 6; // Adjust the factor (6) to control the effect
-  var shadowY = (-offsetY + parentRect.height / 2) / 6; // Adjust the factor (6) to control the effect
-  document.documentElement.style.setProperty('--sX', shadowX + 'px');
-  document.documentElement.style.setProperty('--sY', shadowY + 'px');
-}
-
-document.addEventListener('mousemove', update);
+bay.create('bay-modal', modal, ['open']);
